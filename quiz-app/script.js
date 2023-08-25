06bbcd9f16a0c6dd7ca8ca1735db1fdf783b7524
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-08-25 18:33:29 +0530

    doing some changes

---
+++ quiz-app/script.js
@@ -0,0 +1,36 @@
+const quizdata =[
+
+    {
+        question : "who is the prime minister of india ",
+        a:'ajay',
+        b:'ankit',
+        c:'modi',
+        d:'anil g',
+        correct: 'c'
+    },
+    {
+        question : "best language in the world ",
+        a:'java',
+        b:'c++',
+        c:'js',
+        d:'python',
+        correct: 'c'
+    },
+    {
+        question : "who is the cm of up ",
+        a:'ajay',
+        b:'ankit',
+        c:'yogi',
+        d:'anil g',
+        correct: 'c'
+    },
+    {
+        question : "who is your sister",
+        a:'ajay',
+        b:'ankit',
+        c:'riya',
+        d:'anil g',
+        correct: 'c'
+    },
+     
+]
